cc62e66f62e7f9c5987476f7f3f03fd773ce81f1
Bipan Bhatta <bipan.bhatta420@gmail.com>
2024-09-26 15:25:09 +0000

    terraform etags example. Also added some

--- s3/etags/main.tf
+++ s3/etags/main.tf
@@ -14,13 +14,14 @@ resource "aws_s3_bucket" "default" {
 
 # create objects
 resource "aws_s3_object" "object" {
-  bucket = resource.aws_s3_bucket.default
+  bucket = aws_s3_bucket.default.id
   key    = "myfile.txt"
   source = "myfile.txt"
 
   # The filemd5() function is available in Terraform 0.11.12 and later
   # For Terraform 0.11.11 and earlier, use the md5() function and the file() function:
   # etag = "${md5(file("path/to/file"))}"
-  #etag = filemd5("path/to/file")
+  #etag will notice the changes made to the file
+  etag = filemd5("myfile.txt")
 }
 
